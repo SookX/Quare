fd5cb08f90c56d1d987efc23ed3e870d96f7481e
anastassow <127148449+anastassow@users.noreply.github.com>
2024-12-08 01:28:54 +0200

    download

--- server/question/urls.py
+++ server/question/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import question
+from .views import question, send_pdf
 
 urlpatterns = [
     path('question/', question , name = "question"),
+    path('download/', send_pdf, name='send-pdf'),
 ]
--- server/question/views.py
+++ server/question/views.py
@@ -1,9 +1,32 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from django.http import HttpResponse
+import io
+from reportlab.pdfgen import canvas
+
+
+# def create_pdf():
+#     buffer = io.BytesIO()
+#     p = canvas.Canvas(buffer)
+#     p.drawString(100, 750, "This PDF!")
+#     p.showPage()
+#     p.save()
+#     buffer.seek(0)
+#     return buffer
+
 
 @api_view(['POST'])
 def question(request):
+    
+    """
+    Handles POST requests to process a list of questions and their respective answers.
+
+    This endpoint expects a JSON payload with a "body" key containing a list of question-answer pairs.
+    It processes these pairs, performs required operations, and returns a response indicating 
+    successful processing along with any additional data (if applicable).
+    """
+    
     if request.method == 'POST':
         try:
             questions_list = request.data.get("body", [])
@@ -20,3 +43,15 @@ def question(request):
             return Response({"message": "Questions processed successfully!", "data": sc_response})   
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        
+@api_view(['POST'])
+def send_pdf(request):
+    """
+    Handles POST requests, generates a PDF, and sends it to the frontend.
+    """
+    
+    pdf_buffer = create_pdf()
+
+    response = HttpResponse(pdf_buffer, content_type='application/pdf')
+    response['Content-Disposition'] = 'attachment; filename="generated.pdf"'
+    return response
